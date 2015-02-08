844914d87ab6b7326851ccbfead57129db87fdce
Jeff Ingram <jingramgbr@gmail.com>
2015-02-07 21:27:51 -0600

    Added a user id to tasks, configured user task associations

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,18 @@
 class TasksController < InheritedResources::Base
 
+before_action :authenticate_user!
+ 
+def index
+  @tasks = current_user.tasks
+  respond_with(@tasks)
+end
+ 
+def create
+  @task = current_user.tasks.new(task_params)
+  @task.save
+  respond_with(@task)
+end
+
   private
 
     def task_params
